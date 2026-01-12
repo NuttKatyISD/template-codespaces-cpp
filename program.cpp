531ec9a4d44e48821bfc83d199a4cd2d6e119070
Ryan <ryancnutt@katyisd.org>
2026-01-12 12:43:33 -0600

    Give 'em a little bit of scaffolding

--- program.cpp
+++ program.cpp
@@ -2,18 +2,7 @@
 #include <iostream>
 
 int main() {
-    // Print a simple message
-    std::cout << "Hello, World!"; 
-
-    // Print a message followed by a new line
-    std::cout << "This is on a new line." << std::endl; 
-    
-    // You can also use the newline character '\n'
-    std::cout << "Another line with a newline character.\n"; 
-
-    // Print variables
-    int age = 25;
-    std::cout << "My age is: " << age << std::endl;
+    // Start your lab here
 
     return 0;
 }
